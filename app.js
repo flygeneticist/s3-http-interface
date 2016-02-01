6b5345ad2fef67dd5b0ca2415adfce0b6e38c029
Kevin Keller <flygeneticist@gmail.com>
2016-02-01 11:27:11 -0600

    added AWS SDK and node-uuid

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
-// set the app's port, host, and other basic settings
+// user defined settings
 var host = '127.0.0.1';
 var port = 8888;
 
+// server config and setup
 var http = require('http');
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/views'));
 app.set('view engine', 'jade');
 
-// view routes
+// Load the AWS SDK and UUID
+var AWS = require('aws-sdk');
+var uuid = require('node-uuid');
+
+// Create an S3 client
+var s3 = new AWS.S3();
+
+// Create view routes
 app.get('/', function(req, res) {
   res.render('index.html');
 });
 
-// start up the node server
+// Start up the node server
 app.listen(port, host, function (){
   console.log('Server running at http://'+host+':'+port+'/');
 });
