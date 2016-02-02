8f8b6c2c2570e3c8639c3f0a7deeea1edde4f558
Kevin Keller <flygeneticist@gmail.com>
2016-02-02 12:48:55 -0600

    Search functionality added and refined, 500 error added, && style tweaks.

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ var handlebars = require('express-handlebars') .create({ defaultLayout:'main' })
 app.use(express.static(__dirname + '/public'));
 
 // setup AWS middleware
+
+var multiparty = require('multiparty');
+var util = require('util');
 var qs = require('querystring');
 var AWS = require('aws-sdk');
 var uuid = require('node-uuid');
@@ -23,50 +26,88 @@ var s3 = new AWS.S3();
 // APP ROUTES
 app.get('/', function (req, res) {
   s3.listBuckets(function (err, data) {
-    if (err)
-      res.render(err);
-    else
+    if (err) {
+      console.log(err);
+      res.render('500');
+    } else {
       res.render('index', { buckets: data });
-    });
+    }
+  });
 });
 
 // upload files to S3
 app.post('/', function (req, res) {
-  var body = '';
-
-  req.on('data', function (data) {
-    body += data;
-    // Too much POST data, kill the connection!
-    // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
-    if (body.length > 1e6)
-        req.connection.destroy();
+  var form = new multiparty.Form();
+  var keyName;
+  var bucketName;
+  form.on('field', function(name, value) {
+    if (name === 'bucketName') {
+      keyName = value;
+    } else if (name === 'keyName') {
+      bucketName = value;
+    }
   });
-
-  req.on('end', function () {
-    var post = qs.parse(body);
-    var params = {  Bucket: post.bucketPath,
-                    Key: post.fileKey,
-                    Body: post.fileKey //stream
-                  };
-    s3.putObject(params, function (err, data) {
-      if (err)
-        res.render(err);
-      else
-        res.render('upload', { bucket: params['Bucket'], key: params['Key'] });
+  form.on('part', function(part) {
+    s3.putObject({
+      Bucket: bucketName,
+      Key: keyName,
+      ACL: 'public-read',
+      Body: part,
+      ContentLength: part.byteCount,
+    }, function(err, data) {
+      if (err) {
+        console.log(err);
+        res.render('500');
+      } else {
+        console.log("done", data);
+        res.end("OK");
+        console.log("https://s3.amazonaws.com/" + bucket + '/' + destPath);
+      }
     });
   });
+  form.parse(req);
+  res.render('upload', { bucket: bucketName, key: keyName });
 });
 
 // get search results from S3
 app.get('/search', function (req, res) {
-  res.send('You searching in the wrong place foo.')
+  var params = {
+    Bucket: req.query.bucketName, /* required */
+    MaxKeys: 10,
+    Prefix: req.query.prefixPath
+  };
+  s3.listObjectVersions(params, function(err, data) {
+    if (err) {
+      console.log(err, err.stack);
+      res.render('500');
+    } else {
+      console.log(data);
+
+      if (req.query.keyName != '') {
+        for (i = 0; i < data.Versions.length; i++) {
+          version = data.Versions[i];
+          if (version.Key.indexOf(req.query.keyName) <= 0) {
+            data.Versions.splice(i, 1);
+            i--;
+          }
+        }
+      }
+      res.render('search', {bucket: req.query.bucketName,
+                            prefix: req.query.prefixPath,
+                            file: req.query.keyName,
+                            results: data});  // successful response
+    }
+  });
 });
 
 // custom 404 page
-app.use(function (req, res, next) {
-  res.type('text/plain');
-  res.status(404);
-  res.send('404 - Not Found');
+app.use(function (req, res) {
+  res.render('404');
+});
+
+// custom 500 page
+app.use(function (req, res) {
+  res.render('500');
 });
 
 
