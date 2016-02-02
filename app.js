b6885bd6eaadbb363686a09bc3ee8e3a97ec8178
Kevin Keller <flygeneticist@gmail.com>
2016-02-02 08:41:16 -0600

    major overhaul to project structure to bring into standards

--- app.js
+++ app.js
@@ -6,23 +6,32 @@ var port = 8888;
 var http = require('http');
 var express = require('express');
 var app = express();
+
+// set up handlebars view engine
+var handlebars = require('express-handlebars') .create({ defaultLayout:'main' });
+    app.engine('handlebars', handlebars.engine);
+    app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/views'));
+
+// setup AWS middleware
 var qs = require('querystring');
 var AWS = require('aws-sdk');
 var uuid = require('node-uuid');
 var s3 = new AWS.S3();
 
-app.use(express.static(__dirname + '/views'));
-app.set('view engine', 'jade');
 
 // APP ROUTES
 app.get('/', function (req, res) {
   s3.listBuckets(function (err, data) {
     if (err)
       res.render(err);
-  });
-  res.render('index.html', data);
+    else
+      res.render('index', { buckets: data });
+    });
 });
 
+// upload files to S3
 app.post('/upload', function (req, res) {
   var body = '';
 
@@ -36,22 +45,32 @@ app.post('/upload', function (req, res) {
 
   req.on('end', function () {
     var post = qs.parse(body);
-    var params = {  Bucket: post['bucketPath'],
-                    Key: post['fileKey.name'],
-                    Body: post['fileKey.data'] //stream
+    var params = {  Bucket: post.bucketPath,
+                    Key: post.fileKey,
+                    Body: post.fileKey //stream
                   };
-    s3.putObject(params, function (err, data) {
-      if (err)
-        res.send(err);
-      else
-        res.send("Successfully uploaded data to " + params['Bucket'] + "/" + params['Key']);
-    });
+    res.send(post);
+    // s3.putObject(params, function (err, data) {
+    //   if (err)
+    //     res.send(err);
+    //   else
+    //     res.send("Successfully uploaded data to " + params['Bucket'] + "/" + params['Key']);
+    // });
   });
 });
 
+// get search results from S3
 app.get('/search', function (req, res) {
   res.send('You searching in the wrong place foo.')
 });
+
+// custom 404 page
+app.use(function (req, res, next) {
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not Found');
+});
+
 
 // Start up the node server
 app.listen(port, host, function (){
