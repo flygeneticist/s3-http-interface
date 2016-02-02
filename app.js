86c64d37fc418ff8c8df7269a1dfe3bfe6f9227a
Kevin Keller <flygeneticist@gmail.com>
2016-02-02 16:09:11 -0600

    added custom handlebars helper, refactored search filter logic, and cleaned up misc areas

--- app.js
+++ app.js
@@ -8,13 +8,15 @@ var express = require('express');
 var app = express();
 
 // set up handlebars view engine
-var handlebars = require('express-handlebars') .create({ defaultLayout:'main' });
-    app.engine('handlebars', handlebars.engine);
-    app.set('view engine', 'handlebars');
+var handlebars = require('express-handlebars').create({
+  defaultLayout:'main',
+  helpers: { dateFormater: function (d) { return new Date(d).toString('MM-dd-yy'); }}
+});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '/public'));
 
 // setup AWS middleware
-
 var multiparty = require('multiparty');
 var util = require('util');
 var qs = require('querystring');
@@ -81,18 +83,7 @@ app.get('/search', function (req, res) {
       console.log(err, err.stack);
       res.render('500');
     } else {
-      console.log(data);
-      // remove invalid items based on given key name
-      if (req.query.keyName != '') {
-        for (i = 0; i < data.Versions.length; i++) {
-          version = data.Versions[i];
-          if (version.Key.indexOf(req.query.keyName) < 0) {
-            data.Versions.splice(i, 1);
-            i--;
-          }
-        }
-      }
-      // remove invalid items based on given dates
+      // setup date parameters
       var start = new Date('01/01/1900');
       var end = Date.now();
       if (req.query.startDate) {
@@ -101,22 +92,36 @@ app.get('/search', function (req, res) {
       if (req.query.endDate) {
         end = new Date(req.query.endDate);
       }
+
+      // attach temp signedURL for all remaining files
       for (i = 0; i < data.Versions.length; i++) {
         version = data.Versions[i];
+        // remove invalid items based on given dates
         if (version.LastModified < start || version.LastModified > end) {
           data.Versions.splice(i, 1);
           i--;
         }
-      }
-      // attach temp signedURL for all remaining files
-      for (i = 0; i < data.Versions.length; i++) {
-        var signedUrl = s3.getSignedUrl('getObject', { Bucket: req.query.bucketName, Key: data.Versions[i].Key, Expires: 60 });
-        data.Versions[i]['url'] = signedUrl;
+        // remove invalid items based on given key name
+        else if (req.query.keyName != '' && version.Key.indexOf(req.query.keyName) < 0) {
+          data.Versions.splice(i, 1);
+          i--;
+        }
+        // remove invalid folder items based on trailing slash in key name
+        else if (version.Key.slice(-1) == "/") {
+          data.Versions.splice(i, 1);
+          i--;
+        }
+
+        else {
+          // generate a temporary signed URL for downloading
+          var signedUrl = s3.getSignedUrl('getObject', { Bucket: req.query.bucketName, Key: version.Key, Expires: 60 });
+          data.Versions[i]['url'] = signedUrl;
+        }
       }
       res.render('search', {bucket: req.query.bucketName,
                             prefix: req.query.prefixPath,
                             file: req.query.keyName,
-                            results: data});  // successful response
+                            results: data});
     }
   });
 });
