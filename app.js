53dff2b52b24c95e6a418c599fe09513f7853f81
Kevin Keller <flygeneticist@gmail.com>
2016-02-02 14:39:45 -0600

    Added download capability via generation of temporary signed URLs.

--- app.js
+++ app.js
@@ -82,15 +82,36 @@ app.get('/search', function (req, res) {
       res.render('500');
     } else {
       console.log(data);
-
+      // remove invalid items based on given key name
       if (req.query.keyName != '') {
         for (i = 0; i < data.Versions.length; i++) {
           version = data.Versions[i];
-          if (version.Key.indexOf(req.query.keyName) <= 0) {
+          if (version.Key.indexOf(req.query.keyName) < 0) {
             data.Versions.splice(i, 1);
             i--;
           }
         }
+      }
+      // remove invalid items based on given dates
+      var start = new Date('01/01/1900');
+      var end = Date.now();
+      if (req.query.startDate) {
+        start = new Date(req.query.startDate);
+      }
+      if (req.query.endDate) {
+        end = new Date(req.query.endDate);
+      }
+      for (i = 0; i < data.Versions.length; i++) {
+        version = data.Versions[i];
+        if (version.LastModified < start || version.LastModified > end) {
+          data.Versions.splice(i, 1);
+          i--;
+        }
+      }
+      // attach temp signedURL for all remaining files
+      for (i = 0; i < data.Versions.length; i++) {
+        var signedUrl = s3.getSignedUrl('getObject', { Bucket: req.query.bucketName, Key: data.Versions[i].Key, Expires: 60 });
+        data.Versions[i]['url'] = signedUrl;
       }
       res.render('search', {bucket: req.query.bucketName,
                             prefix: req.query.prefixPath,
